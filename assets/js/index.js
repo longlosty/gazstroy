21f0a5d5cc58169f069fff0b9d80ada2ba455503
Sergei <kinechi1721@gmail.com>
2022-08-31 18:21:48 +0400

    catalog, about, map and footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,7 +19,7 @@ dropdownLink.addEventListener("click", function() {
 })
 
 // Partners slider
-new Swiper (".mySwiper", {
+new Swiper (".partnersSwiper", {
     slidesPerView: 2,
     scrollbar: {
         el: ".swiper-scrollbar",
@@ -28,3 +28,63 @@ new Swiper (".mySwiper", {
         delay: 2500,
       },
 });
+
+// About
+const time = 1500;
+const step = 1;
+
+function incrementingNum(num, elem) {
+    let result = 0;
+    let t = Math.round(time / (num / step));
+
+    let interval = setInterval( () => {
+        result += step;
+        if (result == num) {
+            clearInterval(interval);
+        }
+        elem.innerHTML = result;
+    },
+    t);
+
+}
+const aboutYears = document.querySelector('#aboutYears');
+// incrementingNum(18, aboutYears);
+
+let scrollPos = window.scrollY;
+let aboutH = document.querySelector('.about').scrollHeight + 200;
+
+window.addEventListener("scroll", function () {;
+    scrollPos = this.scrollY;
+    let used = true;
+    console.log(scrollPos, aboutH);
+    if (scrollPos >= aboutH && used == true) {
+        used = false;
+        incrementingNum(18, aboutYears);
+    }
+    
+    
+})
+
+// News slider
+var swiper = new Swiper(".newsSwiper", {
+    slidesPerView: 3,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+});
+
+// Footer
+let t;
+function up() {
+	let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+	if(top > 0) {
+		window.scrollBy(0,-100);
+		t = setTimeout('up()',15);
+	} else clearTimeout(t);
+	return false;
+}
+
+
+
+
