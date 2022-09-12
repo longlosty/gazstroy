18c8ebc0fe4332523173dfaa9cd95e25e84df6ef
Sergei <kinechi1721@gmail.com>
2022-09-12 18:46:22 +0400

    upd of structure and slider's work

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,33 +5,33 @@ const body = document.querySelector('body');
 const header = document.querySelector('.page__header');
 const headerInner = document.querySelector('.header__inner');
 
-burger.addEventListener("click", function() {
+burger.addEventListener("click", function () {
     burger.classList.toggle('active');
     menu.classList.toggle('active');
     body.classList.toggle('lock');
     header.classList.toggle('active');
     headerInner.classList.toggle('hide');
-    
+
 })
 
 // Dropdown
 const dropdownLink = document.querySelector('.hasDropdown');
 const dropdown = document.querySelector('.dropdown');
 
-dropdownLink.addEventListener("click", function() {
+dropdownLink.addEventListener("click", function () {
     dropdown.classList.toggle('opened');
     dropdownLink.classList.toggle('opened');
 })
 
 // Partners slider
-new Swiper (".partnersSwiper", {
+new Swiper(".partnersSwiper", {
     slidesPerView: 1,
     scrollbar: {
         el: ".swiper-scrollbar",
     },
     autoplay: {
         delay: 2500,
-      },
+    },
 
     breakpoints: {
         499: {
@@ -49,78 +49,59 @@ new Swiper (".partnersSwiper", {
 });
 
 // News slider
-// let newsWidth = $('.news').width();
-// if (newsWidth > 499) {
-// new Swiper(".newsSwiper", {
-//     loop: true,
-//     slidesPerView: 1,
+const newsGalary = document.querySelectorAll('#news-galary .slide')
 
-//     navigation: {
-//         nextEl: ".news__nav--next",
-//         prevEl: ".news__nav--prev",
-//     },
+const result = ([...newsGalary]).map(el => ({
+    title: el.querySelector('.news__title').innerText,
+    text: el.querySelector('.news__text').innerText,
+    img: el.querySelector('.news__bg').src
+}))
 
-//     breakpoints: {
-//         499: {
-//             slidesPerView: 1,
-//         },
+const track = document.querySelector('.slider-dots-track')
 
-//         899: {
-//             slidesPerView: 2,
-//         },
+var mySwiper = new Swiper('.newsSwiper', {
+    pagination: {
+        el: track,
+        spaceBetween: 0,
+        clickable: true,
+        centeredSlides: true,
+        renderBullet: function (index, className) {
+            return `<div class = "block ${className}">
+                <div class = "news__inner">
+                    <div class="news__inner--title">${result[index].title}</div>
+                    <div class="news__inner--text">${result[index].text}</div>
+                    <img src = ${result[index].img} class = "news__inner--bg" />
+                </div>
+            </div>`;
+        }
+    },
+    navigation: {
+        nextEl: '.news__nav--next',
+        prevEl: '.news__nav--prev',
+    },
+})
 
-//         1399: {
-//             slidesPerView: 3,
-//         },
+const dotsUpdated = () => {
+    let curr = document.querySelector('.swiper-pagination-bullet-active')
+    track.style.left = -curr.offsetLeft + 'px'
+}
 
-//     }
-// });
-// } else {
-//     new Swiper(".newsSwiperMbl", {
-//         loop: true,
-//         slidesPerView: 1,
-    
-//         navigation: {
-//             nextEl: ".news__nav--next--mbl",
-//             prevEl: ".news__nav--prev--mbl",
-//         },
-//     });
-// }
-
-
-var newsMenu = ['Slide 1', 'Slide 2', 'Slide 3']
-
-var mySwiper = new Swiper ('.newsSwiper', {
-			pagination: {
-                el: '.slider-dots',
-                spaceBetween: 0,
-                clickable: true,
-                renderBullet: function (index, className) {
-                return `<div class="block ${className}">${newsMenu[index]}</div>`;
-                }
-			},
-			navigation: {
-			nextEl: '.news__nav--next',
-			prevEl: '.news__nav--prev',
-			},
-  })
-
-console.log(mySwiper)
+mySwiper.eventsListeners.paginationUpdate.push(dotsUpdated)
 
 // About
 $(document).ready(function () {
     $("#phone").mask("+7 (999)999-99-99", {
-    autoclear: false
-});
+        autoclear: false
+    });
 });
 
 // Footer
 let t;
 function up() {
-	let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
-	if(top > 0) {
-		window.scrollBy(0,-100);
-		t = setTimeout('up()',15);
-	} else clearTimeout(t);
-	return false;
+    let top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+    if (top > 0) {
+        window.scrollBy(0, -100);
+        t = setTimeout('up()', 15);
+    } else clearTimeout(t);
+    return false;
 }
