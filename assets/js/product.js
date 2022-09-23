aa7940a6502a50ffd8f9b93308abb2aad4e781b0
Sergei <kinechi1721@gmail.com>
2022-09-23 18:06:27 +0400

    cart upd

--- assets/js/product.js
+++ assets/js/product.js
@@ -163,3 +163,49 @@ new Swiper(".popularSwiper", {
     // }
 })
 
+
+// Add to card
+
+const productButton = document.querySelectorAll('#addToCart');
+const cartPopup = document.querySelector('.cart__popup--content');
+const cartQuantity = document.querySelector('#cartQuantity');
+const productQuantity = document.querySelector('#productQuantity');
+const fullPrice = document.querySelector('#fullprice');
+
+let price = 0;
+
+function randomId() {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
+
+function priceWithoutSpaces(str) {
+    return str.replace(/\s/g, '');
+}
+
+function normalPrice(str) {
+    return String(str).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1');
+}
+
+const generateCartProduct = (img, title, quantity, price, id) => {
+    return `
+
+        <div class="popup_card" data-id="${id}">
+            <div class="card_preview">
+                <img src="${img}" alt="">
+            </div>
+            <div class="card_info">
+                <p class="card_title">${title}</p>
+                <p class="card_price">
+                <span id="productQuantity">${quantity}</span>х ${price}</p>
+            </div>
+            <div class="card_delete">
+                <button>
+                    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M11.5235 9.75613C12.0122 10.245 12.0122 11.0363 11.5235 11.5238C11.2798 11.7675 10.9598 11.89 10.6396 11.89C10.3196 11.89 9.99965 11.7675 9.75589 11.5238L5.99971 7.76738L2.24353 11.5238C1.99977 11.7675 1.67979 11.89 1.35981 11.89C1.03961 11.89 0.719631 11.7675 0.475872 11.5238C-0.0127907 11.0363 -0.0127907 10.245 0.475872 9.75613L4.23228 5.99995L0.475872 2.24377C-0.0127907 1.75488 -0.0127907 0.963634 0.475872 0.476116C0.964764 -0.0125465 1.75463 -0.0125465 2.24353 0.476116L5.99971 4.23252L9.75589 0.476116C10.2448 -0.0125465 11.0346 -0.0125465 11.5235 0.476116C12.0122 0.963634 12.0122 1.75488 11.5235 2.24377L7.76713 5.99995L11.5235 9.75613Z" fill="#FAFAFA"/>
+                    </svg>
+                </button>
+            </div>
+        </div>
+
+    `;
+}
