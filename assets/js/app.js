dc2df94cf40dea6b780fdfad115709f49944a1c2
Sergei Nikiforov <kinechi1721@gmail.com>
2022-09-27 18:52:16 +0400

    linked pages + cart adaptive

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,96 +39,6 @@ function up() {
     return false;
 }
 
-/* PRODUCT PAGE*/
-
-// Stepper
-const stepper = document.querySelector('.stepper');
-const stepperInput = document.querySelector('#stepperInput');
-const stepUp = document.querySelector('#stepUp');
-const stepDown = document.querySelector('#stepDown');
-
-let count = stepperInput.value;
-
-stepperInput.addEventListener('keyup', (e) => {
-    let self = e.currentTarget;
-
-    self.style.width = `${self.value.length + 6.4}ex`
-
-    if (self.value == '0') {
-        self.value = 1;
-    }
-
-    count = stepperInput.value;
-    
-    if (count == 1) {
-        stepDown.classList.add('disabled');
-    } else {
-        stepDown.classList.remove('disabled');
-    }
-})
-
-stepperInput.addEventListener('change', (e) => {
-    let self = e.currentTarget;
-
-    if (!self.value) {
-        self.value = 1;
-    }
-
-    count = stepperInput.value;
-
-    if (count == 1) {
-        stepDown.classList.add('disabled');
-    } else {
-        stepDown.classList.remove('disabled');
-    }
-
-    self.style.width = `${self.value.length + 6.4}ex`
-})
-
-stepUp.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    count = stepperInput.value;
-    count++;
-    stepperInput.value = count;
-
-    stepperInput.style.width = `${stepperInput.value.length + 6.4}ex`
-
-    if (count == 1) {
-        stepDown.classList.add('disabled');
-    } else {
-        stepDown.classList.remove('disabled');
-    }
-})
-
-stepDown.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    count = stepperInput.value;
-    count--;
-    stepperInput.value = count;
-
-    stepperInput.style.width = `${stepperInput.value.length + 6.4}ex`
-
-    if (count == 1) {
-        stepDown.classList.add('disabled');
-    } else {
-        stepDown.classList.remove('disabled');
-    }
-})
-
-function allowNumbersOnly(e) {
-    var code = (e.which) ? e.which : e.keyCode;
-
-    if (code > 31 && (code < 48 || code > 57)) {
-        e.preventDefault();
-    }
-}
-
-stepperInput.addEventListener('keypress', (e) => {
-    allowNumbersOnly(e);
-})
-
 // Tabs
 const tabBtn = document.querySelectorAll('.tab-btn');
 const tabContent = document.querySelectorAll('.tab-content');
@@ -193,4 +103,229 @@ const generateCartProduct = (img, title, quantity, price, id) => {
 
     `;
 }
+
+
+/* Cart */
+
+ // Прослушиваем загрузку документа
+ document.addEventListener("DOMContentLoaded", function () {
+    // Обновляем страницу при перевороте экрана
+    window.addEventListener("orientationchange", function () {
+      window.location.reload();
+    });
+
+    // Обновить стоимость во всех карточках корзины
+    function updateCartCardsSummary() {
+        // Получаем все карточки на странице корзины
+        const cards = document.querySelectorAll("[data-cart=card]");
+
+        // Проверка на пустоту
+        if (cards.length > 0) {
+        // Проходимся по каждой карточке
+        cards.forEach((card) => {
+            // Получаем актуальную цену
+            let price = card.querySelector("[data-cart=price]").textContent;
+            // Получаем актуальное количество
+            let quantity = card.querySelector("[data-cart=quantity]").value;
+            // Получаем контейнер для стоимости
+            const summaryElement = card.querySelector("[data-cart=summary]");
+
+            // Удаляем все пробелы
+            price = price.replace(/\s/g, "");
+            quantity = quantity.replace(/\s/g, "");
+
+            // Преобразуем число
+            price = parseInt(price);
+            quantity = parseInt(quantity);
+
+            // if (!Number.isInteger(quantity) || quantity < 0) {
+            //   quantity = 0;
+            // }
+
+            // Рассчитываем финальную стоимость
+            let summaryValue = price * quantity;
+            // Разделяем на разряды
+            summaryValue = summaryValue.toLocaleString();
+            // Подставляем значение в нужный элемент
+            summaryElement.textContent = summaryValue;
+        });
+        }
+    }
+
+    // Обновляем блок с финальным расчетом стоимости
+    function updateCartResult() {
+        // Контейнер для финальный результат
+        const resultFinal = document.querySelector("[data-cart=result-final]");
+
+        // Получаем все карточки на странице и сразу преобразуем коллекцию в массив
+        const cardsAll = [...document.querySelectorAll("[data-cart=card]")];
+        // Фильтруем карточки (не должно быть display:none)
+        const cardsFiltered = cardsAll.filter((card) => getComputedStyle(card).display != "none");
+
+        // Получаем блок с заглушкой
+        const thumbEmpty = document.querySelector("[data-cart=thumb-empty]");
+
+        // Резервируем переменную для значения
+        let resultValue = 0;
+
+        // Проверка на пустоту
+        if (cardsFiltered.length > 0) {
+            // Собираем стоимость из всех карточек на странице
+            cardsFiltered.forEach((card) => {
+            const cardSummary = card.querySelector("[data-cart=summary]");
+            if (cardSummary) {
+                let value = cardSummary.textContent;
+                value = value.replace(/\s/g, "");
+                value = parseInt(value);
+
+                resultValue = resultValue + value;
+            }
+            });
+
+            // Проверка на пустоту
+            if (thumbEmpty) {
+            // Скрываем блок есть карточки есть
+            thumbEmpty.style.display = "none";
+            }
+        } else {
+            // Проверка на пустоту
+            if (thumbEmpty) {
+            // Показываем блок если карточек нет
+            thumbEmpty.style.display = "block";
+            }
+        }
+
+        // Проверка на пустоту
+        if (resultFinal) {
+            // Присваиваем значение и делим по разрядам
+            resultFinal.textContent = resultValue.toLocaleString();
+        }
+    }
+
+    // Корзина
+    document.addEventListener("click", function (event) {
+    if (
+        // При клике на кнопку увеличить или уменьшить кол-во товара (в карточке)
+        event.target.closest(".quantity-button") ||
+        // При клике на кнопку удалить карточку из корзины
+        event.target.closest("[data-cart=remove]") ||
+        // Клике на кнопку удалить все товары из корзины
+        event.target.closest("[data-cart=remove-all]")
+        ) {
+        setTimeout(function () {
+            // Обновляем стоимость в карточках корзины
+            updateCartCardsSummary();
+            // Обновляем суммарные стоимость в корзине
+            updateCartResult();
+        }, 25);
+        }
+    })
+
+    updateCartCardsSummary();
+    updateCartResult();
+
+    /* PRODUCT PAGE AND CART*/
+    // Stepper
+    const stepper = document.querySelector('.stepper');
+    const stepperInput = document.querySelector('#stepperInput');
+    const stepUp = document.querySelector('#stepUp');
+    const stepDown = document.querySelector('#stepDown');
+
+    let count = stepperInput.value;
+
+    stepperInput.addEventListener('keyup', (e) => {
+        let self = e.currentTarget;
+
+        self.style.width = `${self.value.length + 6.4}ex`
+
+        if (self.value == '0') {
+            self.value = 1;
+        }
+
+        count = stepperInput.value;
+        
+        if (count == 1) {
+            stepDown.classList.add('disabled');
+        } else {
+            stepDown.classList.remove('disabled');
+        }
+    })
+
+    stepperInput.addEventListener('change', (e) => {
+        let self = e.currentTarget;
+
+        if (!self.value) {
+            self.value = 1;
+        }
+
+        count = stepperInput.value;
+
+        if (count == 1) {
+            stepDown.classList.add('disabled');
+        } else {
+            stepDown.classList.remove('disabled');
+        }
+
+        self.style.width = `${self.value.length + 6.4}ex`;
+
+        updateCartCardsSummary();
+        updateCartResult();
+    })
+
+    stepUp.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        count = stepperInput.value;
+        count++;
+        stepperInput.value = count;
+
+        stepperInput.style.width = `${stepperInput.value.length + 6.4}ex`
+
+        if (count == 1) {
+            stepDown.classList.add('disabled');
+        } else {
+            stepDown.classList.remove('disabled');
+        }
+
+        updateCartCardsSummary();
+        updateCartResult();
+    })
+
+    stepDown.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        count = stepperInput.value;
+        count--;
+        stepperInput.value = count;
+
+        stepperInput.style.width = `${stepperInput.value.length + 6.4}ex`
+
+        if (count == 1) {
+            stepDown.classList.add('disabled');
+        } else {
+            stepDown.classList.remove('disabled');
+        }
+
+        updateCartCardsSummary();
+        updateCartResult();
+    })
+
+    function allowNumbersOnly(e) {
+        var code = (e.which) ? e.which : e.keyCode;
+
+        if (code > 31 && (code < 48 || code > 57)) {
+            e.preventDefault();
+        }
+    }
+
+    stepperInput.addEventListener('keypress', (e) => {
+        allowNumbersOnly(e);
+        
+    })
+
+});
+
+
+
+
 
